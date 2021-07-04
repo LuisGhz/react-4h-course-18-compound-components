3872f63982a3c247150b9440f37fcda84690dab5
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 14:48:17 -0500

    Import Body, Title and Text Extends Card with those components

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Image } from "./styles/card";
+import { Container, Image, Body, Title, Text } from "./styles/card";
 
 const Card = ({ children, ...restProps }) => {
   return <Container { ...restProps } >{ children }</Container>
@@ -6,6 +6,18 @@ const Card = ({ children, ...restProps }) => {
 
 Card.Image = ({ src, alt, ...restProps }) => {
   return <Image src={ src } alt={ alt } { ...restProps } />
+}
+
+Card.Title = ({ children, ...restProps }) => {
+  return <Title { ...restProps } >{ children }</Title>
+}
+
+Card.Body = ({ children, ...restProps }) => {
+  return <Body { ...restProps } >{ children }</Body>
+}
+
+Card.Text = ({ children, ...restProps }) => {
+  return <Text { ...restProps } >{ children }</Text>
 }
 
 export default Card;
