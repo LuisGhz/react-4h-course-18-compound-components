ab96f004d8d3d487394caa7bdb78d87234e05408
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-04 14:41:53 -0500

    Set styles to Container and Create Image component Set text align center and max width of 250px to Container Set width 100% to image

--- src/components/card/styles/card.jsx
+++ src/components/card/styles/card.jsx
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 export const Container = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
+  text-align: center;
   margin: 1rem;
-
+  max-width: 250px;
   &:hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   }
 `;
+
+export const Image = styled.img`
+  width: 100%;
+`
